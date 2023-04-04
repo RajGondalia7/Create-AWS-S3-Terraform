010e7ad81a43ce383762c63d3b1b1e42c6fe90af
RajGondalia7 <114596788+RajGondalia7@users.noreply.github.com>
2023-04-04 13:21:39 +0100

    Final

---
+++ create s3.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "var.region"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
+}
+
+resource "aws_s3_bucket" "d"{
+  bucket = "sadda"
+  acl = "private"
+  
+  versioning {
+	enabled = true
+  }
+
+}
